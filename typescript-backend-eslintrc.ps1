3302f187fc79d9620ed8035d85499704379bf115
Marko Harjula <marko.harjula@avanio.com>
2024-06-24 09:27:44 +0300

    updates

--- typescript-backend-eslintrc.ps1
+++ typescript-backend-eslintrc.ps1
@@ -11,7 +11,21 @@ Write-Output "setup ${type} eslint"
 Write-Output "download eslint-configs template from github"
 Invoke-WebRequest -URI 'https://raw.githubusercontent.com/mharj/eslint-configs/main/typescript-backend-eslintrc.json' -OutFile '.eslintrc.json'
 Write-Output "install eslint packages"
-Invoke-Expression "$NpmCmd i -D eslint@8 eslint-config-standard prettier eslint-config-prettier eslint-plugin-deprecation eslint-plugin-prettier eslint-plugin-sonarjs@0.23 @typescript-eslint/parser @typescript-eslint/eslint-plugin @stylistic/eslint-plugin"
+
+$EslintPackageList = @(
+  "eslint@8",
+  "eslint-config-standard",
+  "prettier",
+  "eslint-config-prettier",
+  "eslint-plugin-deprecation",
+  "eslint-plugin-prettier",
+  "eslint-plugin-sonarjs@0.23",
+  "@typescript-eslint/parser",
+  "@typescript-eslint/eslint-plugin",
+  "@stylistic/eslint-plugin"
+)
+
+Invoke-Expression "$NpmCmd i -D $($EslintPackageList -join ' ')"
 if (-not(Test-Path 'tsconfig.test.json')) {
   Write-Output "tsconfig.test.json not found, eslint needs it to work properly!"
   return
