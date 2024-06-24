b8a7c587f27af40afa79e9ee41efa1faef08bc7c
Marko Harjula <marko.harjula@avanio.com>
2024-06-24 09:19:56 +0300

    update

--- typescript-backend-eslintrc.ps1
+++ typescript-backend-eslintrc.ps1
@@ -1,12 +1,17 @@
+$NpmCmd = "npm"
 if (-not(Test-Path 'package.json')) {
   Write-Output "package.json not found, not valid node project!"
   return
+}
+# check if pnpm-lock.yaml exists and change npm command to pnpm
+if (Test-Path 'pnpm-lock.yaml') {
+  $NpmCmd = "pnpm"
 }
 Write-Output "setup ${type} eslint"
 Write-Output "download eslint-configs template from github"
 Invoke-WebRequest -URI 'https://raw.githubusercontent.com/mharj/eslint-configs/main/typescript-backend-eslintrc.json' -OutFile '.eslintrc.json'
 Write-Output "install eslint packages"
-npm i -D eslint@8 eslint-config-standard prettier eslint-config-prettier eslint-plugin-deprecation eslint-plugin-prettier eslint-plugin-sonarjs@0.23 @typescript-eslint/parser @typescript-eslint/eslint-plugin @stylistic/eslint-plugin
+. "$NpmCmd i -D eslint@8 eslint-config-standard prettier eslint-config-prettier eslint-plugin-deprecation eslint-plugin-prettier eslint-plugin-sonarjs@0.23 @typescript-eslint/parser @typescript-eslint/eslint-plugin @stylistic/eslint-plugin"
 if (-not(Test-Path 'tsconfig.test.json')) {
   Write-Output "tsconfig.test.json not found, eslint needs it to work properly!"
   return
--- typescript-backend.ps1
+++ typescript-backend.ps1
@@ -1,7 +1,20 @@
+$NpmCmd = "npm"
 if (-not(Test-Path 'package.json')) {
   Write-Output "package.json not found, not valid node project!"
   return
 }
+# check if pnpm-lock.yaml exists and change npm command to pnpm
+if (Test-Path 'pnpm-lock.yaml') {
+  $NpmCmd = "pnpm"
+}
+
+$BackendPackageList = @(
+  "typescript",
+  "@types/node",
+  "ts-node",
+  "source-map-support"
+)
+
 Write-Output "setup ${type} typescript"
 Write-Output "install typescript packages"
-npm i -D typescript @types/node ts-node source-map-support
+. "$NpmCmd i -D $($BackendPackageList -join ' ')"
